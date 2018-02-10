41722232da5e15aca87d4c4571fabad560859344
mttsui <xzs603@gmail.com>
2018-02-10 18:33:08 +0800

    加入dev plugin

--- src/main/java/com/mttsui/hr/web/HelloController.java
+++ src/main/java/com/mttsui/hr/web/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("")
-    public String hello(String name){
-        return "hello" + name;
+    public String hello(){
+        return "hello";
     }
 }
