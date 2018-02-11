6374315964c0eccd6aeeb7afdb10f7f92a318a98
mttsui <xzs603@gmail.com>
2018-02-11 09:00:40 +0800

    dtocontroller

---
+++ src/main/java/com/mttsui/hr/common/dto/PageDataResp.java
@@ -0,0 +1,8 @@
+package com.mttsui.hr.common.dto;
+
+/**
+ * @Author: zsxu2
+ * @Date 2018/2/11 8:39
+ */
+public class PageDataResp {
+}
---
+++ src/main/java/com/mttsui/hr/web/DtoController.java
@@ -0,0 +1,8 @@
+package com.mttsui.hr.web;
+
+/**
+ * @Author: zsxu2
+ * @Date 2018/2/11 8:29
+ */
+public class DtoController {
+}
